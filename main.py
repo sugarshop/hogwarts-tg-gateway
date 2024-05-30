ccdd13600a5c98b98db3e2a70f31a5c34ad104b0
RQ <mengmengmengqiang@gmail.com>
2024-05-31 01:42:20 +0800

    [feat]: register handler wallet_address_subscribe_handler & subscribe_address_conv_handler & conv_cancel_handler

--- main.py
+++ main.py
@@ -9,13 +9,21 @@ from handlers import (
     address_transactions_handler, 
     start_handler,
     inlineKeyboard_button_handler,
-    help_handler
+    help_handler,
+    conv_cancel_handler,
+    subscribe_address_conv_handler,
+    wallet_address_subscribe_handler
 )
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
 
 TG_BOT_TOKEN = os.environ.get('TG_BOT_TOKEN', 'empty value')
 
@@ -27,5 +35,8 @@ if __name__ == '__main__':
     application.add_handler(address_transactions_handler)
     application.add_handler(inlineKeyboard_button_handler)
     application.add_handler(help_handler)
+    application.add_handler(conv_cancel_handler)
+    application.add_handler(subscribe_address_conv_handler)
+    application.add_handler(wallet_address_subscribe_handler)
     
     application.run_polling(allowed_updates=Update.ALL_TYPES)
